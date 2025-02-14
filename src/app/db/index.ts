314a172fb88eb9bbaeb72f3ed407a9a1f4ec5250
modjohnsmith <qaplatformmoderator@gmail.com>
2025-02-14 20:37:19 +0300

    Update src/app/db/index.ts
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- src/app/db/index.ts
+++ src/app/db/index.ts
@@ -1,5 +1,5 @@
 import { drizzle } from "drizzle-orm/neon-http";
 import { neon } from "@neondatabase/serverless";
 
-const sql = neon("postgresql://neondb_owner:npg_4bnwEKAzcPI3@ep-quiet-flower-a82czq76-pooler.eastus2.azure.neon.tech/neondb?sslmode=require");
+const sql = neon(process.env.DATABASE_URL);
 export const db = drizzle(sql);
